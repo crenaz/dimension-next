2e97cccd38fc6a2bb37fdce3ef559126462472b2
crenaz <crenaz@gmail.com>
2026-09-07 15:09:20 -0400

    security: upgrade to Next.js 16 and React 19 with 0 vulnerabilities

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  // Add Partytown configuration
+  experimental: {
+    nextScriptWorkers: true,
+  },
+};
+
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,107 +1,119 @@
 import Head from "next/head";
-import Script from 'next/script';
+import Script from "next/script";
 import React from "react";
 
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
 
-import { Partytown } from '@builder.io/partytown/react';
+import { Partytown } from "@builder.io/partytown/react";
 
 class IndexPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isArticleVisible: false,
-            timeout: false,
-            articleTimeout: false,
-            article: "",
-            loading: "is-loading"
-        }
-        this.handleOpenArticle = this.handleOpenArticle.bind(this)
-        this.handleCloseArticle = this.handleCloseArticle.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      isArticleVisible: false,
+      timeout: false,
+      articleTimeout: false,
+      article: "",
+      loading: "is-loading",
+    };
+    this.handleOpenArticle = this.handleOpenArticle.bind(this);
+    this.handleCloseArticle = this.handleCloseArticle.bind(this);
+  }
+
+  componentDidMount() {
+    this.timeoutId = setTimeout(() => {
+      this.setState({ loading: "" });
+    }, 100);
+  }
+
+  componentWillUnmount() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
     }
+  }
 
-    componentDidMount() {
-        this.timeoutId = setTimeout(() => {
-            this.setState({ loading: "" })
-        }, 100)
-    }
+  handleOpenArticle(article) {
+    this.setState({
+      isArticleVisible: !this.state.isArticleVisible,
+      article,
+    });
 
-    componentWillUnmount() {
-        if (this.timeoutId) {
-            clearTimeout(this.timeoutId)
-        }
-    }
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout,
+      });
+    }, 325);
 
-    handleOpenArticle(article) {
-        this.setState({
-            isArticleVisible: !this.state.isArticleVisible,
-            article
-        })
+    setTimeout(() => {
+      this.setState({
+        articleTimeout: !this.state.articleTimeout,
+      });
+    }, 350);
+  }
 
-        setTimeout(() => {
-            this.setState({
-                timeout: !this.state.timeout
-            })
-        }, 325)
+  handleCloseArticle() {
+    this.setState({
+      articleTimeout: !this.state.articleTimeout,
+    });
 
-        setTimeout(() => {
-            this.setState({
-                articleTimeout: !this.state.articleTimeout
-            })
-        }, 350)
-    }
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout,
+      });
+    }, 325);
 
-    handleCloseArticle() {
-        this.setState({
-            articleTimeout: !this.state.articleTimeout
-        })
+    setTimeout(() => {
+      this.setState({
+        isArticleVisible: !this.state.isArticleVisible,
+        article: "",
+      });
+    }, 350);
+  }
+  render() {
+    return (
+      <div
+        className={`body ${this.state.loading} ${
+          this.state.isArticleVisible ? "is-article-visible" : ""
+        }`}
+      >
+        <div>
+          <Head>
+            <title>Dimension Next</title>
+            <link
+              href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i"
+              rel="stylesheet"
+            />
+            <Partytown
+              debug={process.env.NODE_ENV === "development"}
+              forward={["dataLayer.push"]}
+            />
+          </Head>
+          <Script src="your-third-party-script-url" type="text/partytown" />
 
-        setTimeout(() => {
-            this.setState({
-                timeout: !this.state.timeout
-            })
-        }, 325)
+          <style />
 
-        setTimeout(() => {
-            this.setState({
-                isArticleVisible: !this.state.isArticleVisible,
-                article: ""
-            })
-        }, 350)
-    }
-    render() {
-        return (
-            <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? "is-article-visible" : ""}`}>
-                <div>
-                    <Head>
-                        <title>Dimension Next</title>
-                        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i" rel="stylesheet" />
-                        <Partytown debug={true} forward={['dataLayer.push']} />
-                    </Head>
-                        <Script src="#" type="text/partytown" />
-                    
+          <div id="wrapper">
+            <Header
+              onOpenArticle={this.handleOpenArticle}
+              timeout={this.state.timeout}
+            />
+            <Main
+              isArticleVisible={this.state.isArticleVisible}
+              timeout={this.state.timeout}
+              articleTimeout={this.state.articleTimeout}
+              article={this.state.article}
+              onCloseArticle={this.handleCloseArticle}
+            />
+            <Footer timeout={this.state.timeout} />
+          </div>
 
-                    <style />
-
-                    <div id="wrapper">
-                        <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
-                        <Main
-                            isArticleVisible={this.state.isArticleVisible}
-                            timeout={this.state.timeout}
-                            articleTimeout={this.state.articleTimeout}
-                            article={this.state.article}
-                            onCloseArticle={this.handleCloseArticle}
-                        />
-                        <Footer timeout={this.state.timeout} />
-                    </div>
-
-                    <div id="bg" />
-                </div>
-            </div>
-        )
-    }
+          <div id="bg" />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default IndexPage
+export default IndexPage;
